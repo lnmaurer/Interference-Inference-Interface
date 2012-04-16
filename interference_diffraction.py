46808622d3222052305dfc94e78327ee2ebdf09a
Leon Maurer <leon.maurer@gmail.com>
2012-04-16 17:42:52 -0500

    Code cleanup and move to finding intensity with the DFT method (yeilding a 4-5x speedup)

--- interference_diffraction.py
+++ interference_diffraction.py
@@ -1,7 +1,9 @@
 import Tkinter, tkFileDialog
 import ttk
 import tkMessageBox
-import numpy as np
+import time
+#import numpy as np
+from numpy import *
 from PIL import Image, ImageTk
 
 class Interface:
@@ -11,8 +13,8 @@ class Interface:
   epsilon0 = 8.854187817620e-12 # Vacuum permittivity
   c        = 1/(mu0*epsilon0)**0.5
   
-  Nx = 800 #width of simulation and view canvas
-  Ny = 400 #height of simulation and view canvas
+  Nx = 600 #width of simulation and view canvas
+  Ny = 300 #height of simulation and view canvas
   barrierX = 100 #x position of the barrier
   
   d  = 2.0/Nx
@@ -47,14 +49,15 @@ class Interface:
 #for the simulation
     self.t = 0 #will hold the current time
     self.cont = False #the simulation isn't currently running
-    self.Ez = np.zeros((self.NEZx, self.NEZy,28)) #3rd dimension to keep track of past values of Ez. todo: change it's number to make appropriate to find Ez_rms
-    self.Hx = np.zeros((self.NHXx, self.NHXy))
-    self.Hy = np.zeros((self.NHYx, self.NHYy))
+    self.Ez = zeros((self.NEZx, self.NEZy,3)) #3rd dimension to keep track of past values of Ez.
+    self.EzDTF = zeros((self.NEZx, self.NEZy))
+    self.Hx = zeros((self.NHXx, self.NHXy))
+    self.Hy = zeros((self.NHYx, self.NHYy))
     
     #for testing only
     self.lamb = 20*self.d
-    self.k = 2*np.pi/self.lamb
-    self.omega = 2*np.pi*self.c/self.lamb
+    self.k = 2*pi/self.lamb
+    self.omega = 2*pi*self.c/self.lamb
     self.tau = self.lamb/self.c
     
     print str(self.tau/self.dt)
@@ -91,14 +94,17 @@ class Interface:
     self.viewFrame = ttk.Labelframe(self.root, text='View')
     self.viewFrame.grid(column=0,row=0,sticky='nsew',padx=5,pady=5)
     
-    self.canvas = Tkinter.Canvas(self.viewFrame, width=self.Nx, height=self.Ny)
-    self.canvas.grid(column=1, row=0, columnspan=3, rowspan=3, sticky='nsew', padx=5, pady=5)    
+    self.Ezcanvas = Tkinter.Canvas(self.viewFrame, width=self.Nx, height=self.Ny)
+    self.Ezcanvas.grid(column=0, row=0, columnspan=3, rowspan=3, sticky='nsew', padx=5, pady=5)    
+
+    self.EzRMScanvas = Tkinter.Canvas(self.viewFrame, width=self.Nx, height=self.Ny)
+    self.EzRMScanvas.grid(column=0, row=3, columnspan=3, rowspan=3, sticky='nsew', padx=5, pady=5)        
     
 #The barrier frame frame and intial barrier setup
     self.barrierFrame = ttk.Labelframe(self.root, text='Barrier')
     self.barrierFrame.grid(column=1,row=0,sticky='nsew',padx=5,pady=5)
 
-    self.gaps = [[50,75],[200,250]]
+    self.gaps = [[50,70],[230,250]]
     
     ttk.Button(self.barrierFrame, text='Add Opening').grid(column=0, row=0,sticky='nsew', padx=5, pady=5) #todo: add command
 
@@ -128,7 +134,7 @@ class Interface:
     setFreqLabel() #todo: not working; starts at zero
   
   
-    self.redrawCanvas();
+    self.redrawCanvases();
   
   def updateEzPlot(self):
     #the numpy array; just for testing purposes
@@ -136,21 +142,27 @@ class Interface:
     #1)the only floats Image.fromstring can handle are 32bit, so need to do that conversion
     #2)0 (and below) are black, 255 and above are white, shades of gray inbetween
     #3)need to store array in (height, width) format
-    data = np.float32((np.transpose(self.Ez[:,:,0])+1)/2*256)
+    data = float32((transpose(self.Ez[:,:,0])+1)/2*256)
     im = Image.fromstring('F', (data.shape[1], data.shape[0]), data.tostring())
     self.ezPlot = ImageTk.PhotoImage(image=im) #need to store it so it doesn't get garbage collected, otherwise it won't display correctly on the canvas
     
   def updateEzRMSPlot(self):
-    data = np.float32(np.transpose(np.sqrt(np.sum(np.square(self.Ez),axis=2)/28)))*256*(2**0.5) #RMS of Ez
+    if self.EzDTF[0,0] != 0.0:
+      data = 256*(float32(transpose(square(abs(self.EzDTF))))/abs(self.EzDTF[0,0])**2)
+    else: #the data is zero, so just display it
+      data = float32(transpose(abs(self.EzDTF)))
     im = Image.fromstring('F', (data.shape[1], data.shape[0]), data.tostring())
     self.ezRMSPlot = ImageTk.PhotoImage(image=im) #need to store it so it doesn't get garbage collected, otherwise it won't display correctly on the canvas
     
-  def redrawCanvas(self):
-    #first, clear everything off the canvas (but don't delete the canvas itself)
-    self.canvas.delete('all')
+  def redrawCanvases(self):
+    #first, clear everything off the canvases (but don't delete the canvases themselves)
+    self.Ezcanvas.delete('all')
+    self.EzRMScanvas.delete('all')
     
     self.updateEzPlot()
-    self.canvas.create_image(0,0,image=self.ezRMSPlot,anchor=Tkinter.NW)    
+    self.Ezcanvas.create_image(0,0,image=self.ezPlot,anchor=Tkinter.NW)      
+    self.updateEzRMSPlot()
+    self.EzRMScanvas.create_image(0,0,image=self.ezRMSPlot,anchor=Tkinter.NW)    
     
     #next, draw the barrier
     invGaps = [ypos for pair in self.gaps for ypos in pair] #flatten self.gaps
@@ -158,13 +170,16 @@ class Interface:
     invGaps.append(self.Ny) #put Ny for the last item
     #now invGaps looks like [0, start of first gap, end of first gap, start of second gap, end of second gap,...,Ny]
     for i in range(0,len(invGaps),2):
-      self.canvas.create_line([(self.barrierX,invGaps[i]),(self.barrierX,invGaps[i+1])], width=1, fill='red')    
-  
+      self.Ezcanvas.create_line([(self.barrierX,invGaps[i]),(self.barrierX,invGaps[i+1])], width=1, fill='red')    
+      self.EzRMScanvas.create_line([(self.barrierX,invGaps[i]),(self.barrierX,invGaps[i+1])], width=1, fill='red')    
+
+      
   def reset():
     pass
     #todo: enable frequency slider
     #todo: set t=0
     #todo: clear the canvas
+    #todo: clear Ez (and make it the right length???)
   
   def start(self):
     #todo: reset if it's never been run before
@@ -178,18 +193,25 @@ class Interface:
     
   def run(self):
     if self.cont:
+      t = time.clock()
       self.step()
-      self.redrawCanvas()
+      self.redrawCanvases()
+      print str(time.clock()-t)
       self.root.after(1,self.run) #todo: adjust time???
   
   def step(self):
+    #so that we don't need a bazillion "self."s
+    Ez = self.Ez
+    Hx = self.Hx
+    Hy = self.Hy
+    
     #first, handle the Ez source -- area from the barrier left
     #x and y for points on the barrier and to the left
-    x, y = self.d * np.mgrid[self.EZx_ex_range, self.EZy_ex_range]
+    x, y = self.d * mgrid[self.EZx_ex_range, self.EZy_ex_range]
     tr = self.t - x/self.c
     #want wave to start gradually and propigate at speed of light
     #logistic growth makes it come in gradually and use of retarded time there and in step function at end enforces propigation
-    self.Ez[self.EZx_ex_range, self.EZy_ex_range, 0] = np.sin(self.k*x-self.omega*self.t)/(1+np.exp(-(tr-3*self.tau)/self.tau))*((tr > 0).astype(float))
+    Ez[self.EZx_ex_range, self.EZy_ex_range, 0] = sin(self.k*x-self.omega*self.t)/(1+exp(-(tr-3*self.tau)/self.tau))*((tr > 0).astype(float))
 
     #now, enforce Ez=0 on barrier
     invGaps = [ypos for pair in self.gaps for ypos in pair] #flatten self.gaps
@@ -197,18 +219,18 @@ class Interface:
     invGaps.append(self.Ny) #put Ny for the last item
     #now invGaps looks like [0, start of first gap, end of first gap, start of second gap, end of second gap,...,Ny]
     for i in range(0,len(invGaps),2):
-      self.Ez[self.barrierX, invGaps[i]:invGaps[i+1], 0] = 0     
+      Ez[self.barrierX, invGaps[i]:invGaps[i+1], 0] = 0     
     
     
     #next, take care of Hx and Hy using the standard Yee algorithm
-    self.Hx[self.HXx_range, self.HXy_range] = self.Hx[self.HXx_range, self.HXy_range] + self.Db*(self.Ez[self.HXx_range, self.HXy_range, 0] - self.Ez[self.HXx_range, 1:(self.NHXy+1), 0]) #HXy_range+1
-    self.Hy[self.HYx_range, self.HYy_range] = self.Hy[self.HYx_range, self.HYy_range] + self.Db*(self.Ez[1:(self.NHYx+1), self.HYy_range, 0] - self.Ez[self.HYx_range, self.HYy_range, 0]) #HYx_range+1
+    Hx[self.HXx_range, self.HXy_range] = Hx[self.HXx_range, self.HXy_range] + self.Db*(Ez[self.HXx_range, self.HXy_range, 0] - Ez[self.HXx_range, 1:(self.NHXy+1), 0]) #HXy_range+1
+    Hy[self.HYx_range, self.HYy_range] = Hy[self.HYx_range, self.HYy_range] + self.Db*(Ez[1:(self.NHYx+1), self.HYy_range, 0] - Ez[self.HYx_range, self.HYy_range, 0]) #HYx_range+1
     
     #move old Ez back in the array
-    self.Ez = np.roll(self.Ez,1,axis=2)
+    Ez = roll(Ez,1,axis=2)
     
     #do the normal Yee updates on Ez in the relevant range
-    self.Ez[self.EZx_range, self.EZy_range, 0] = self.Ez[self.EZx_range, self.EZy_range, 1] + self.Cb*(self.Hy[self.EZx_range, self.EZy_range] - self.Hy[self.barrierX:(self.NEZx-2),self.EZy_range] + self.Hx[self.EZx_range, 0:self.NEZy-2] - self.Hx[self.EZx_range, self.EZy_range])
+    Ez[self.EZx_range, self.EZy_range, 0] = Ez[self.EZx_range, self.EZy_range, 1] + self.Cb*(Hy[self.EZx_range, self.EZy_range] - Hy[self.barrierX:(self.NEZx-2),self.EZy_range] + Hx[self.EZx_range, 0:self.NEZy-2] - Hx[self.EZx_range, self.EZy_range])
     
     
     ##now take care of the Mur RBCs
@@ -219,25 +241,31 @@ class Interface:
     rng   = slice(1,self.NEZy-1) #range of everything in y except the corners
     rngp1 = slice(2,self.NEZy)
     rngm1 = slice(0,self.NEZy-2)
-    self.Ez[-1,rng,0]  = -self.Ez[-2,rng,2] + Ma*(self.Ez[-2,rng,0] + self.Ez[-1,rng,2]) + Mb*(self.Ez[-1,rng,1] + self.Ez[-2,rng,1]) + Mc*(self.Ez[-1,rngp1,1] - 2*self.Ez[-1,rng,1] + self.Ez[-1,rngm1,1] + self.Ez[-2,rngp1,1] - 2*self.Ez[-2,rng,1] + self.Ez[-2,rngm1,1])
+    Ez[-1,rng,0]  = -Ez[-2,rng,2] + Ma*(Ez[-2,rng,0] + Ez[-1,rng,2]) + Mb*(Ez[-1,rng,1] + Ez[-2,rng,1]) + Mc*(Ez[-1,rngp1,1] - 2*Ez[-1,rng,1] + Ez[-1,rngm1,1] + Ez[-2,rngp1,1] - 2*Ez[-2,rng,1] + Ez[-2,rngm1,1])
 
     #for y=0
-    rng = slice(self.barrierX+2,self.NEZx-1) #range of everything in x except the corners
+    rng = slice(self.barrierX+2,self.NEZx-1) #range of everything in x except right corner
     rngp1 = slice(self.barrierX+3,self.NEZx)
     rngm1 = slice(self.barrierX+1,self.NEZx-2)
-    self.Ez[rng,0,0]  = -self.Ez[rng,1,2] + Ma*(self.Ez[rng,1,0] + self.Ez[rng,0,2]) + Mb*(self.Ez[rng,0,1] + self.Ez[rng,1,1]) + Mc*(self.Ez[rngp1,0,1] - 2*self.Ez[rng,0,1] + self.Ez[rngm1,0,1] + self.Ez[rngp1,1,1] - 2*self.Ez[rng,1,1] + self.Ez[rngm1,1,1])
+    Ez[rng,0,0]  = -Ez[rng,1,2] + Ma*(Ez[rng,1,0] + Ez[rng,0,2]) + Mb*(Ez[rng,0,1] + Ez[rng,1,1]) + Mc*(Ez[rngp1,0,1] - 2*Ez[rng,0,1] + Ez[rngm1,0,1] + Ez[rngp1,1,1] - 2*Ez[rng,1,1] + Ez[rngm1,1,1])
 
     #for y=NEZy
-    self.Ez[rng,-1,0]  = -self.Ez[rng,-2,2] + Ma*(self.Ez[rng,-2,0] + self.Ez[rng,-1,2]) + Mb*(self.Ez[rng,-1,1] + self.Ez[rng,-2,1]) + Mc*(self.Ez[rngp1,-1,1] - 2*self.Ez[rng,-1,1] + self.Ez[rngm1,-1,1] + self.Ez[rngp1,-2,1] - 2*self.Ez[rng,-2,1] + self.Ez[rngm1,-2,1])
+    Ez[rng,-1,0]  = -Ez[rng,-2,2] + Ma*(Ez[rng,-2,0] + Ez[rng,-1,2]) + Mb*(Ez[rng,-1,1] + Ez[rng,-2,1]) + Mc*(Ez[rngp1,-1,1] - 2*Ez[rng,-1,1] + Ez[rngm1,-1,1] + Ez[rngp1,-2,1] - 2*Ez[rng,-2,1] + Ez[rngm1,-2,1])
 
     #now for the corners
-    self.Ez[-1,0,0] = self.Ez[-2,1,2] #bottom right
-    self.Ez[-1,-1,0] = self.Ez[-2,-2,2] #top right
+    Ez[-1,0,0] = Ez[-2,1,2] #bottom right
+    Ez[-1,-1,0] = Ez[-2,-2,2] #top right
     
     ##finially, update the time and the sources todo: don't have to update the sources twice?
     self.t = self.t + self.dt
-    self.Ez[self.EZx_ex_range, self.EZy_ex_range, 0] = np.sin(self.k*x-self.omega*self.t)/(1+np.exp(-(tr-3*self.tau)/self.tau))*((tr > 0).astype(float))
-  
+    Ez[self.EZx_ex_range, self.EZy_ex_range, 0] = sin(self.k*x-self.omega*self.t)/(1+exp(-(tr-3*self.tau)/self.tau))*((tr > 0).astype(float))
+    
+    #strictly speaking, the following will blow up to infinity if you integrate forever (since the FT of a sinusoid is a delta function)
+    #however, we're not that patient. plus, floating point limitations will prevent it (once the numbers are large enough, adding a small number to them won't change them)
+    self.EzDTF = self.EzDTF + Ez[:,:,0]*(cos(self.omega*self.t) + 1j*sin(self.omega*self.t))
+    self.Ez = Ez
+    self.Hx = Hx
+    self.Hy = Hy  
   
   
   
